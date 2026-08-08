ab5990fd9197046d11877cdf954c9dddf34250e1
Didier Domínguez <dadu0699@hotmail.com>
2026-08-08 14:35:58 -0600

    refactor: Remove unnecessary @Repository annotation from SlackChannelWhitelistRepository

--- src/main/java/com/blacklisthub/repository/SlackChannelWhitelistRepository.java
+++ src/main/java/com/blacklisthub/repository/SlackChannelWhitelistRepository.java
@@ -1,13 +1,11 @@
 package com.blacklisthub.repository;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
 
 import com.blacklisthub.entity.SlackChannelWhitelistEntity;
 
 import reactor.core.publisher.Mono;
 
-@Repository
 public interface SlackChannelWhitelistRepository
         extends ReactiveCrudRepository<SlackChannelWhitelistEntity, Long> {
 
